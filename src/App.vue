59d8b2bf1522958e4ea8bbde6ff047f5594200e5
Joel McCandless <mail@joelmccandless.com>
2025-03-05 11:39:54 -0500

    start creating homepages

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,97 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="flex-row">
+      <img alt="Joel McCandless Logo" class="logo" src="@/assets/logo.png" height="40px"/>
+      <h1>Joel McCandless</h1>
     </div>
+      <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
   </header>
 
-  <RouterView />
+  <div class="content">
+    <RouterView />
+  </div>
+
+  <footer class="flex-row">
+    <div class="flex-row">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-envelope" viewBox="0 0 16 16">
+        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+      </svg>
+    </div>
+    <p>&copy; 2025 Joel McCandless.</p>
+  </footer>
+
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+  position: sticky;
+  top: 0;
+  z-index: 1000;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  background: white;
+  border-bottom: 1px solid #ddd;
+  max-height: 100px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h1 {
+  margin: 0;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  padding-left: 1rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  font-size: 1.2rem;
+  display: flex;
+  gap: 1rem;
+  align-items: center;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+  transition: text-decoration 0.2s;
 }
 
-nav a:first-of-type {
-  border: 0;
+nav a:hover {
+  text-decoration: underline;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+nav a.router-link-exact-active {
+  color: var(--logo-blue);
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.content {
+  margin-top: 100px;
+  padding: 1rem;
+  max-width: 1200px;
+  margin: 0 auto;
+  flex-grow: 1;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+footer {
+  padding: 1rem;
+  justify-content: space-between;
+  background: var(--logo-blue);
+  border-top: 1px solid #ddd;
+  margin-top: 1rem;
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+footer p {
+  margin: 0;
+  color: white;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="flex-row id-card">
+      <div class="portrait">
+        <img src="@/assets/images/park.jpeg" alt="Portrait" />
+      </div>
+      <div>
+        <h1>About Me</h1>
+        <p>
+          <b>
+            CS Masters and CS/Computer Engineering Bachelors at Rensselaer Polytechnic Institute
+          </b>
+        </p>
+        <p>
+          Hi, I am a CS Masters student studying the computation behind intelligent behavior, including methods of Machine Learning, Computer Vision, and Control Systems.
+          I enjoy taking a multidisciplinary approach to solve complex problems, pursuing ambitious goals to address modern needs.
+        </p>
+        <p>
+          Explore my website to learn more about my projects, research, and interests!
+        </p>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+  .portrait {
+    width: 100%;
+    max-width: 300px;
+    margin: 0 auto;
+    padding: 0 20px;
+  }
+  .portrait img {
+    width: 100%;
+    border-radius: 50%;
+  }
+  .id-card {
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    gap: 20px;
+    margin: 60px auto;
+    max-width: 1000px;
+  }
+
+  b {
+    font-weight: 700;
+    font-size: large;
+  }
+
+  p {
+    font-size: medium;
+    margin-bottom: 10px;
+  }
+</style>
