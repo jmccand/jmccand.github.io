8a4698fc91a87fff4e037aaa44e4c5fdf0299c39
Joel McCandless <mail@joelmccandless.com>
2025-03-14 23:10:40 -0400

    add RPI logo to research view

--- src/views/ResearchView.vue
+++ src/views/ResearchView.vue
@@ -2,4 +2,15 @@
 </script>
 
 <template>
+    <img class="rpi-image" src="@/assets/images/rpi-logo.png" alt="Research" />
 </template>
+
+<style scoped>
+    .rpi-image {
+        position: sticky;
+        top: 150px;
+        width: 100%;
+        margin: 0 auto;
+        padding: 0 20px;
+    }
+</style>
