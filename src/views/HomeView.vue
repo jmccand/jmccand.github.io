020c4ff6d7c658ccc88890e7974066baa1842cb7
Joel McCandless <mail@joelmccandless.com>
2025-05-07 22:05:51 -0400

    finish adding featured to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,18 +3,19 @@ import homepageData from '@/assets/data/homepage.json';
 import projectsData from '@/assets/data/projects.json';
 import researchData from '@/assets/data/research.json';
 import { ref } from 'vue';
+import { RouterLink } from 'vue-router'
 
 const featuredTitles = homepageData.featured;
 var features = [];
+researchData.forEach((research) => {
+  if (featuredTitles.includes(research.name)) {
+    features.push(["research", research.name, research.images[0]])
+  }
+});
 projectsData.forEach((project) => {
   console.log(project);
   if (featuredTitles.includes(project.name)) {
-    features.push(["projects", project.name, project.versions[0].images[0], project.details]);
-  }
-});
-researchData.forEach((research) => {
-  if (featuredTitles.includes(research.name)) {
-    features.push(["research", research.name, research.images[0], research.details])
+    features.push(["projects", project.name, project.versions[0].images[0]]);
   }
 });
 const featureIndex = ref(0);
@@ -55,21 +56,27 @@ const getImageUrl = (feature) => {
     <div class="flex-row mission-featured">
       <div class="mission">
         <h1>Mission Statement</h1>
-        I will maintain a boundless curiosity for learning by embracing the excitement found in exploration. I will acquire both a breadth and depth of skills and unite fields in novel ways. I will maintain a standard for myself which builds self-esteem and pride in my accomplishments. I will give off gratitude for life and form friendships with those around me in an effort to grow together.
+        <div class="mission-statement">
+          <svg class="begin-quote" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M6 6a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3H5a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2zm9 0a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3h-1a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2z" clip-rule="evenodd"/></svg>
+          <div class="mission-body">I will maintain a boundless curiosity for learning by embracing the excitement found in exploration. I will acquire both a breadth and depth of skills and unite fields in novel ways. I will maintain a standard for myself which builds self-esteem and pride in my accomplishments. I will give off gratitude for life and form friendships with those around me in an effort to grow together.</div>
+          <svg class="end-quote" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M6 6a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3H5a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2zm9 0a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h3a3 3 0 0 1-3 3h-1a1 1 0 1 0 0 2h1a5 5 0 0 0 5-5V8a2 2 0 0 0-2-2z" clip-rule="evenodd"/></svg>
+        </div>
       </div>
       <div class="featured">
-        <div>
+        <div class="feature-arrow-container">
           <svg v-if="featureIndex > 0" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256" @click="featureIndex--"><path fill="#aaa" d="M168.49 199.51a12 12 0 0 1-17 17l-80-80a12 12 0 0 1 0-17l80-80a12 12 0 0 1 17 17L97 128Z"></path></svg>
         </div>
         <div class="featured-content">
-          <div>
+          <h2>Featured</h2>
+          <div class="featured-image-container">
             <img :src="getImageUrl(features[featureIndex])" alt="Feature Image" class="feature-image" />
           </div>
-          <p class="feature-description">
-            {{ features[featureIndex][3] }}
-          </p>
+          <div class="feature-text">
+            <p class="feature-name">{{ features[featureIndex][1] }}</p>
+            <RouterLink :to="`/${features[featureIndex][0]}`">Continue to {{ features[featureIndex][0] }} ></RouterLink>
+          </div>
         </div>
-        <div>
+        <div class="feature-arrow-container">
           <svg v-if="featureIndex < features.length - 1" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 256 256" @click="featureIndex++"><path fill="#aaa" d="m184.49 136.49l-80 80a12 12 0 0 1-17-17L159 128L87.51 56.49a12 12 0 1 1 17-17l80 80a12 12 0 0 1-.02 17"></path></svg>
         </div>
       </div>
@@ -108,6 +115,28 @@ const getImageUrl = (feature) => {
     margin-bottom: 10px;
   }
 
+  .mission-statement {
+    border-radius: 10px;
+    background-color: #eee;
+    padding: 15px;
+    margin-top: 15px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .begin-quote {
+    transform: rotate(180deg);
+  }
+
+  .mission-body {
+    width: 90%;
+    align-self: center;
+  }
+
+  .end-quote {
+    align-self: flex-end;
+  }
+
   .down-arrow {
     justify-content: center;
     margin: 20px auto 40px auto;
@@ -139,6 +168,30 @@ const getImageUrl = (feature) => {
   .featured-content {
     display: flex;
     flex-direction: column;
+    gap: 20px;
+    align-items: center;
+  }
+
+  .featured-image-container {
+    display: flex;
+    height: 200px;
+    width: 300px;
+  }
+
+  @keyframes fade {
+    0%, 100% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0.3;
+    }
+  }
+
+  .feature-arrow-container {
+    display: flex;
+    width: 50px;
+    height: 50px;
+    animation: fade 2s infinite ease-in-out;
   }
 
   @keyframes bounce-fade {
@@ -156,13 +209,18 @@ const getImageUrl = (feature) => {
   animation: bounce-fade 2s infinite ease-in-out;
 }
 
-.feature-image {
-  max-height: 200px;
-  margin: auto;
+.feature-text {
+  text-align: center;
 }
 
-.feature-description {
-  max-height: 5rem;
-  overflow: hidden;
+.feature-name {
+  padding: 0;
+  margin: 0;
+}
+
+.feature-image {
+  max-height: 100%;
+  max-width: 100%;
+  margin: auto;
 }
 </style>
